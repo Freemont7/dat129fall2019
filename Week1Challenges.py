309b5307aa0a79acf71dbb9d5a6650f1b941536d
Chris Campbell <freemont7@gmail.com>
2019-09-11 19:47:02 -0400

    add

---
+++ Week1Challenges.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Chris Campbell
+9/11/19
+Python 2 - Dat 129 - Fall 2019
+In Class Challenges
+
+1: Write a program that counts to 100 skipping odd numbers and 
+printing each value one after another 
+(use the end= attribute for calls to print())
+"""
+
+# def main():
+
+#hundred = [range(0, 100)]
+#list(range(10))
+    
+#print(range(0, 10))
+
+for i in range(0, 102, 2):
+    print(i)
+
+    
+kaboom = "KABOOM"
+
+for i in kaboom:
+    
+    print(i*3)
+    
+    
+alice = "askaliceithinkshe'llknow"
+
+for i in alice:
+    print(alice[])
+    
+
